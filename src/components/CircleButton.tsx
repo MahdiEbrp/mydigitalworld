3e2c622e361af897dc3e53bdf8fcac39119240c5
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2023-05-09 15:21:26 +0330

    before optimize and animation

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import { IconType } from 'react-icons';
 
 type CircleButtonProps = {
     children: React.ReactElement<IconType>;
-} & React.HTMLAttributes<HTMLButtonElement>;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const CircleButton: React.FC<CircleButtonProps> = ({ children, ...rest }) => {
     return (
         <button
-            className='flex justify-center items-center w-8 h-8 m-1 hover:animate-pulse rounded-full bg-primaryBackground-800 hover:bg-primaryBackground-700 text-primary-800 transition-colors'
+            className='flex justify-center items-center w-8 h-8 m-1 disabled:opacity-25 rounded-full bg-primaryBackground-800 hover:bg-primaryBackground-700 text-primary-800 transition-all'
             {...rest}
         >
             {React.cloneElement(children)}
