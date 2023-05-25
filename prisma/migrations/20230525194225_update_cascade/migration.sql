d466eb3edd5fc72bcab0de1c559f88bcf34dce80
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2023-05-25 23:13:50 +0330

    update cascade

---
+++ prisma/migrations/20230525194225_update_cascade/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Feedback" DROP CONSTRAINT "Feedback_topicId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Feedback" DROP CONSTRAINT "Feedback_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Gallery" DROP CONSTRAINT "Gallery_topicId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Topic" DROP CONSTRAINT "Topic_userid_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Topic" ADD CONSTRAINT "Topic_userid_fkey" FOREIGN KEY ("userid") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Feedback" ADD CONSTRAINT "Feedback_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Gallery" ADD CONSTRAINT "Gallery_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE CASCADE ON UPDATE CASCADE;
