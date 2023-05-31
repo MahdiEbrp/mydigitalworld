f22ea94debb51a4dcd92a19d7be704afed09e2ea
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2023-06-01 01:28:39 +0330

    test likes on server

---
+++ src/pages/api/like/update.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import prismaClient from '../../../lib/prismaClient';
+
+const prisma = prismaClient;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getSession({ req });
+
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
+    if (!session?.user?.id) {
+        return res.status(401).end();
+    }
+
+    const { isLike, topicId } = <{ isLike: boolean; topicId: string; }>req.body;
+
+    if (typeof isLike === 'undefined' || typeof topicId === 'undefined') {
+        return res.status(400).end();
+    }
+
+    const userId = session.user.id;
+
+    try {
+        const feedbacks = await prisma.feedback.findMany({
+            where: { topicId },
+        });
+
+        let feedback = feedbacks.find(ab => ab.topicId === topicId && ab.userId === userId);
+
+        const updatedValue = feedback ? feedback.isLike === isLike ? null : isLike : isLike;
+
+        if (!feedback) {
+            feedback = await prisma.feedback.create({
+                data: {
+                    isLike,
+                    userId,
+                    topicId,
+                }
+            });
+        } else {
+            feedback = await prisma.feedback.update({
+                where: { id: feedback.id },
+                data: { isLike: updatedValue },
+            });
+        }
+        feedbacks.forEach(fb => {
+            if (fb.id === feedback?.id) {
+                fb.isLike = feedback.isLike;
+            }
+        });
+
+        const { likes, dislikes } = feedbacks.reduce((counts, fb) => {
+            if (fb.isLike === true) counts.likes++;
+            else if (fb.isLike === false) counts.dislikes++;
+            return counts;
+        }, { likes: 0, dislikes: 0 });
+
+        const likedBySessionUser = feedbacks.some(fb => fb.userId === userId && fb.isLike === true);
+        const dislikedBySessionUser = feedbacks.some(fb => fb.userId === userId && fb.isLike === false);
+
+        return res.status(200).json({ likes, dislikes, likedBySessionUser, dislikedBySessionUser });
+
+    } catch (error) {
+        return res.status(500).json({ error: 'Unknown error' });
+    }
+}
--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,43 +1,75 @@
 import FetchError from '@/components/display/FetchError';
+import Head from 'next/head';
+import ImageGallery from '@/components/ImageGallery';
 import Loader from '@/components/display/Loader';
 import NothingToSee from '@/components/display/NothingToSee';
-import ImageGallery from '@/components/ImageGallery';
-import { SignInModalContext } from '@/context/SignInContext';
-import { useToast } from '@/context/ToastContext';
-import useCachedAxios from '@/lib/cachedAxios';
-import getHumorousHTTPMessage from '@/lib/humorousHTTPMessage';
-import { Gallery } from '@/type/gallery';
 import axios, { AxiosError } from 'axios';
+import getHumorousHTTPMessage from '@/lib/humorousHTTPMessage';
+import useCachedAxios from '@/lib/cachedAxios';
+import React, { useContext, useEffect, useState } from 'react';
 import { NextPage } from 'next';
+import { SignInModalContext } from '@/context/SignInContext';
 import { useSession } from 'next-auth/react';
-import Head from 'next/head';
-import { useContext, useState } from 'react';
+import { useToast } from '@/context/ToastContext';
+import { Gallery } from '@/type/gallery';
 
-const Gallery: NextPage = () => {
+type LikeResponse = {
+    likes: number,
+    dislikes: number,
+    likedBySessionUser: boolean,
+    dislikedBySessionUser: boolean;
+};
+
+const GalleryPage: NextPage = () => {
     const { data: galleryData, error: galleryError, isLoading: isGalleryLoading } = useCachedAxios<Gallery[]>('GALLERY_DATA', '/api/gallery');
     const { setModalVisibility } = useContext(SignInModalContext);
     const [loadingTopicIds, setLoadingTopicIds] = useState<string[]>([]);
+    const [updatedGalleryData, setUpdatedGalleryData] = useState<Gallery[] | undefined>(undefined);
     const { data: session } = useSession();
     const toast = useToast();
 
-    const handleLikeButton = async (id: string) => {
+    useEffect(() => {
+        if (galleryData) {
+            setUpdatedGalleryData(galleryData);
+        }
+    }, [galleryData]);
+
+    const handleLikeDislike = async (topicId: string, isLike: boolean) => {
         if (!session) {
             setModalVisibility(true);
             return;
         }
-        setLoadingTopicIds(prevLoadingIds => [...prevLoadingIds, id]);
+        if (!updatedGalleryData)
+            return;
+        setLoadingTopicIds(prevLoadingIds => [...prevLoadingIds, topicId]);
 
         try {
-            await axios.post('/api/like/update', { id, action: 'like' });
+            const { data } = await axios.post('/api/like/update', { topicId: topicId, isLike: isLike });
+            const response = data as LikeResponse;
+            const updatedData = updatedGalleryData.map((image) => {
+                if (image.topicId === topicId) {
+                    return {
+                        ...image,
+                        likes: response.likes,
+                        dislikes: response.dislikes,
+                        likedBySessionUser: response.likedBySessionUser,
+                        dislikedBySessionUser: response.dislikedBySessionUser,
+                    };
+                }
+                return image;
+            });
+            setUpdatedGalleryData(updatedData);
+            if (response.likedBySessionUser)
+                toast.showToast('🥰 Aww, you liked my post! Thank you so much! 🙌', 'success', 2000);
         } catch (error) {
             if (error instanceof AxiosError)
                 toast.showToast(getHumorousHTTPMessage(error.response?.status || 0), 'error', 4000);
             else
                 toast.showToast('Seriously, doesn\'t know what\'s happening.🧐👻', 'error', 2000);
+        } finally {
+            setLoadingTopicIds(prevLoadingIds => prevLoadingIds.filter(topicId => topicId !== topicId));
         }
-        finally {
-            setLoadingTopicIds(prevLoadingIds => prevLoadingIds.filter(topicId => topicId !== id));
-        }
+
     };
 
     return (
@@ -50,7 +82,11 @@ const Gallery: NextPage = () => {
                     <Loader />
                     :
                     <>
-                        {galleryData ? <ImageGallery images={galleryData} onLikeClick={handleLikeButton} loadingTopicIds={loadingTopicIds} /> : galleryError ? <FetchError /> : <NothingToSee />}
+                        {updatedGalleryData ?
+                            <ImageGallery images={updatedGalleryData} onLikeClick={(id) => handleLikeDislike(id, true)} onDislikeClick={(id) => handleLikeDislike(id, false)} loadingTopicIds={loadingTopicIds} />
+                            :
+                            galleryError ? <FetchError /> : <NothingToSee />
+                        }
                     </>
                 }
             </div>
@@ -58,4 +94,4 @@ const Gallery: NextPage = () => {
     );
 };
 
-export default Gallery;
+export default GalleryPage;
